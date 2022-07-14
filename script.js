e4305c565e95562eeb4568bf7c36e41208a646bb
Augusto Ventura <paivaav@gmail.com>
2022-07-14 13:03:00 -0300

    create gameboard module and render it with play samples

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-(function gameboard() {})();
+(function() {
+    let gameboard = {
+        board:[],
+        init: function(){
+            this.cacheDom();
+            this.render();
+        },
+        cacheDom: function(){
+            board = document.querySelectorAll('ul');
+        },
+        render: function(){
+            for (i=0;i<board.length;i++){
+                board[i].addEventListener('click', function (e) {
+                    if (e.target.textContent == "X")
+                        e.target.textContent = "O";
+                    else
+                        e.target.textContent = "X";
+                });
+            };
+        }
+    };
+    gameboard.init();
+})();
+
+function Player(){
+    return Player;
+};
