7d1647f1a4531782277cf1cae7987d715ce0c9d8
Augusto Ventura <paivaav@gmail.com>
2022-07-17 20:42:47 -0300

    create first checkwinner and change players turn

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// const gamerBoard = (function() {
-//     let gameboard = {
-//         board:[],
-//         init: function(){
-//             this.cacheDom();
-//             this.render();
-//         },
-//         cacheDom: function(){
-//             board = document.querySelectorAll('ul');
-//         },
-//         render: function(){
-//             for (i=0;i<board.length;i++){
-//                 board[i].addEventListener('click', e => {
-//                     if (e.target.textContent == "X")
-//                         e.target.textContent = "O";
-//                     else
-//                         e.target.textContent = "X";
-//                 });
-//             };
-//         },
-            
-//     };
-//     gameboard.init();
-// })();
- 
 const gameBoard = (function() {
     let gameboard = {
         board: new Array(9),
@@ -71,9 +46,8 @@ const displayController = (() =>{
     const markCell = (player) => {
         gameCells.forEach( (item, index) =>{
             item.addEventListener('click', e =>{
-                item.textContent = index;
-                // gameEngine.playNow(parseInt(e.target.dataset.index));
-                // updateBoard();
+                 gameEngine.playNow(index);
+                updateBoard();
             });
         });
     };
@@ -92,11 +66,45 @@ const gameEngine = (function(){
     const playerX = Player("X");
     const playerO = Player("O");
     let round = 1;
+    let playerXTurn = true;
     let finished = false;
 
+
     const playNow = (index) => {
-        gameBoard.gameboard.board[index] = index;
+        if(playerXTurn)
+            gameBoard.gameboard.board[index] = playerX.getMark();
+        else
+            gameBoard.gameboard.board[index] = playerO.getMark();
+    
+        if (round >=3){
+            if(checkResult()){
+                return;
+            }
+        }    
+        round++;
+        
+        //next player's turn
+        playerXTurn = (playerXTurn == true) ? false : true;
     };
+    
+    const checkResult = () => {
+        if ( gameBoard.gameboard.board[0]!= null && gameBoard.gameboard.board[0]==gameBoard.gameboard.board[1] && gameBoard.gameboard.board[1]==gameBoard.gameboard.board[2]){
+            displayController.resultMessage("Tuf"); 
+            round = 1;
+            return true;
+        }
+    };
+    
+
     return{playNow};
 
 })();
+
+// 0-1-2
+// 0-4-8
+// 0-3-6
+// 1-4-7
+// 2-4-6
+// 2-5-8
+// 3-4-5
+// 6-7-8
