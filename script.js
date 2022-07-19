aa00463bbdb0ece42735dc5efba17863709f3af2
Augusto Ventura <paivaav@gmail.com>
2022-07-19 20:37:48 -0300

    check first game engine and update result message

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ const gameBoard = (function() {
         setCell: function(index, mark){
             this.board[index] = mark;
             },
-        reset: () => {
-            for(let i=0; i<this.board.length; i++)
+        resetB: function() {
+            for(let i=0; i<this.board.length; i++){
                 this.board[i] = "";
+            }
             }
         }
         return {gameboard};
@@ -32,10 +33,11 @@ const displayController = (() =>{
     let gameCells = document.querySelectorAll('ul');
     let playerTurn = document.querySelector('.player-turn');
     let result = document.querySelector('.result');
+    const btnReset = document.querySelector('.reset');
 
-    const reset = () => {
+    const resetDisplay = () => {
         gameCells.forEach( (item)=> {item.textContent ="";} );
-        playerTurn.textContent = "Player X's turn"; 
+        playerTurn.textContent = "Let's start with player X."; 
         result.textContent = "Game Restarted";
     };
 
@@ -59,14 +61,24 @@ const displayController = (() =>{
     };
       
     const turnMessage = function(turn) {    
-        if(turn)
+        if(turn){
             playerTurn.textContent = "Player X's turn.";
-        else
+        }
+        else{
             playerTurn.textContent = "Player O's turn.";
+        }
     };
 
+    btnReset.addEventListener('click', (e) => {
+       e.preventDefault();
+       gameBoard.gameboard.resetB();
+       gameEngine.resetEngine();
+       resetDisplay();
+       markCell();
+    });
+
     markCell();
-    return {gameCells, reset, turnMessage, resultMessage};
+    return {gameCells, resetDisplay, turnMessage, resultMessage};
 })();
 
 const gameEngine = (function(){
@@ -75,54 +87,79 @@ const gameEngine = (function(){
     let round = 1;
     let playerXTurn = true;
     let finished = false;
-
+    
+    
 
     const playNow = (index) => {
+        while(!finished){
         //it plays only if the field is available.
         if(gameBoard.gameboard.board[index] != null)
             {return;}
         (playerXTurn) ? gameBoard.gameboard.setCell(index, playerX.getMark()):gameBoard.gameboard.setCell(index, playerO.getMark());
         
-        if (round >=3){
+        //check a possible winner from the 5th round on
+        if (round >=5){
             if(checkResult()){
-                return;
+                alert("oooover");
+                return; 
             }
-            if(round == 9){
-                //draw
-            }
+            // if(round == 9){
+            //     finished = true;
+            //     round=1;
+            //     //gameBoard.gameboard.reset();
+            //     //draw
+            // }
         }    
         round++;
         
         //next player's turn
         playerXTurn = (playerXTurn == true) ? false : true;
         displayController.turnMessage(playerXTurn);
-        // }
+    }
     };
     
     const checkResult = () => {
-        if ( gameBoard.gameboard.board[0]!= null && gameBoard.gameboard.board[0]==gameBoard.gameboard.board[1] && gameBoard.gameboard.board[1]==gameBoard.gameboard.board[2]){
-            displayController.resultMessage("Tuf"); 
-            round = 1;
-            return true;
-        }
+        let XMarks = [];
+        let OMarks = [];
+        const winningConditions = [
+            [0,1,2],
+            [0,4,8],
+            [0,3,6],
+            [1,4,7],
+            [2,4,6],
+            [2,5,8],
+            [3,4,5],
+            [6,7,8]
+            ];
+    
+        gameBoard.gameboard.board.forEach((item, index)=>{
+            if(item == "X")
+                XMarks.push(index);
+            if(item == "O")
+                OMarks.push(index);
+        });
+        winningConditions.forEach((wCondition)=> {
+            if(wCondition.every(item => (XMarks.includes(item)))){
+                displayController.resultMessage("X"); 
+                finished = true;
+            }
+            if(wCondition.every(item => (OMarks.includes(item)))){
+                displayController.resultMessage("O"); 
+                finished = true;
+            }
+        });
+        return finished;
     };
     
-    const nextPlayer = (turn)=>{
-        if(turn){
-            turn = false;
-            // displayController.
-        }
-    };
 
-    return{playNow};
+    const resetEngine = () => {
+        round = 1;
+        finished = false;
+        playerXTurn = true;
+    };
+    
+
+    return{playNow, resetEngine, checkResult};
 
 })();
 
-// 0-1-2
-// 0-4-8
-// 0-3-6
-// 1-4-7
-// 2-4-6
-// 2-5-8
-// 3-4-5
-// 6-7-8
